25bc0ca6edf636665dd644cd50b14985660f0dea
Shaleen Baral <shaleenbaral111@gmail.com>
2022-12-17 05:32:16 -0500

    Added a few things

--- util/firebase.ts
+++ util/firebase.ts
@@ -5,7 +5,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup,
 import withFirebaseAuth from "react-with-firebase-auth";
 
 const firebaseConfig = {
-
+  apiKey: "AIzaSyAnPMKfVIKERo0xi_Qx4yDaH0QngGgM0Vo",
+  authDomain: "chatbox-71f08.firebaseapp.com",
+  projectId: "chatbox-71f08",
+  storageBucket: "chatbox-71f08.appspot.com",
+  messagingSenderId: "481405540940",
+  appId: "1:481405540940:web:e1291287f42caf7ac632e9",
+  measurementId: "G-2YJ1CR1HGQ"
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
